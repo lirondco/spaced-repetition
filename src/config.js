bd6b96c1bd6d7361ccea27ff5ec557137fd0708f
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-26 08:36:12 -0800

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   API_ENDPOINT: 'https://thawing-plains-80929.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
